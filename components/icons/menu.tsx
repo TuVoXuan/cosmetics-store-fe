fa42f88698094691aef6d8ee0760e8b6225af1d7
TuVoXuan <voxuantu1235@gmail.com>
2023-01-15 20:14:42 +0700

    feat: make header UI and responsive

--- components/icons/menu.tsx
+++ components/icons/menu.tsx
@@ -4,19 +4,12 @@ function Menu(props: SVGProps<SVGSVGElement>) {
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
-			width={props.width}
-			height={props.height}
+			stroke="currentColor"
 			fill="none"
 			viewBox="0 0 24 24"
 			{...props}
 		>
-			<path
-				stroke={props.color}
-				strokeLinecap="round"
-				strokeLinejoin="round"
-				strokeWidth="2"
-				d="M2 8h20M2 16h20"
-			></path>
+			<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M2 8h20M2 16h20" />
 		</svg>
 	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Header from "../components/header";
+import SearchInput from "../components/inputs/search-input";
 
 export default function Home() {
 	return <Header />;
