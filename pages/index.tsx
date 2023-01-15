388a6110b62018ae53a5745b7e49d2bb31b71ee3
TuVoXuan <voxuantu1235@gmail.com>
2023-01-15 20:28:15 +0700

    fix: update dark logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,12 @@
+import Footer from "../components/footer";
 import Header from "../components/header";
 import SearchInput from "../components/inputs/search-input";
 
 export default function Home() {
-	return <Header />;
+	return (
+		<div>
+			<Header />
+			<Footer />
+		</div>
+	);
 }
