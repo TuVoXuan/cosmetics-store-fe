d7a912a9df47c97d960d589c46c25fd7507de7c7
TuVoXuan <voxuantu1235@gmail.com>
2023-01-20 23:09:29 +0700

    make responsive for categories

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,32 @@
 import Image from "next/image";
 import React, { useRef, useState } from "react";
+import { black_dark, white_light } from "../styles/color";
+import PrimaryBtn from "./buttons/primary-btn";
 import CartIndicator from "./icons/cart-indicator";
-import HeartIcon from "./icons/heart";
+import Delete from "./icons/delete";
+import Facebook from "./icons/facebook";
+import GoBack24 from "./icons/go-back-24";
+import GoForward from "./icons/go-forward";
+import Instargram from "./icons/instargram";
 import Menu from "./icons/menu";
 import Profile from "./icons/profile";
 import Search from "./icons/search";
+import Twitter from "./icons/twitter";
 import SearchInput from "./inputs/search-input";
 
-export default function Header() {
+interface Props {
+	onShowNavbar: () => void;
+}
+
+export default function Header({ onShowNavbar }: Props) {
 	const logoRef = useRef<HTMLDivElement>(null);
+	const navbarRef = useRef<HTMLElement>(null);
+	const navbarListRef = useRef<HTMLUListElement>(null);
+	const headerRef = useRef<HTMLElement>(null);
+	const categoriesRef = useRef<HTMLDivElement>(null);
+
+	const [showNavbar, setShowNavbar] = useState<boolean>(false);
+	// const [showCategories, setShowCategories] = useState<boolean>(false);
 	const [search, setSearch] = useState<boolean>(false);
 
 	const handleClickSearchIcon = () => {
@@ -25,92 +43,265 @@ export default function Header() {
 		}
 	};
 
+	const handleClickNavBarBtn = () => {
+		if (navbarRef.current) {
+			if (navbarRef.current.classList.contains("hidden")) {
+				navbarRef.current.classList.remove("hidden");
+				setShowNavbar(true);
+			} else {
+				navbarRef.current.classList.add("hidden");
+				setShowNavbar(false);
+			}
+
+			if (headerRef.current) {
+				if (!headerRef.current.classList.contains("z-[3]")) {
+					headerRef.current.classList.add("z-[3]");
+				}
+			}
+			onShowNavbar();
+		}
+		if (categoriesRef.current) {
+			if (!categoriesRef.current.classList.contains("hidden")) {
+				categoriesRef.current.classList.add("hidden");
+			}
+		}
+
+		if (window.innerWidth >= 768) {
+			if (navbarListRef.current) {
+				if (navbarListRef.current.classList.contains("-translate-x-[110px]")) {
+					navbarListRef.current.classList.remove("-translate-x-[110px]");
+				}
+
+				if (navbarListRef.current.classList.contains("h-[552px]")) {
+					navbarListRef.current.classList.remove("h-[552px]");
+				}
+			}
+		}
+	};
+
+	const handleShowCategories = (e: any) => {
+		e.stopPropagation();
+
+		if (categoriesRef.current) {
+			if (categoriesRef.current.classList.contains("hidden")) {
+				categoriesRef.current.classList.remove("hidden");
+			} else {
+				categoriesRef.current.classList.add("hidden");
+			}
+		}
+		if (window.innerWidth < 768) {
+			if (headerRef.current) {
+				headerRef.current.classList.remove("z-[3]");
+			}
+		} else {
+			if (categoriesRef.current) {
+				categoriesRef.current.classList.remove(
+					"fixed",
+					"z-[2]",
+					"top-0",
+					"bottom-0",
+					"left-0",
+					"right-0"
+				);
+			}
+			if (navbarListRef.current) {
+				if (navbarListRef.current.classList.contains("-translate-x-[110px]")) {
+					navbarListRef.current.classList.remove("-translate-x-[110px]");
+				} else {
+					navbarListRef.current.classList.add("-translate-x-[110px]");
+				}
+
+				if (!navbarListRef.current.classList.contains("h-[552px]")) {
+					navbarListRef.current.classList.add("h-[552px]");
+				} else {
+					navbarListRef.current.classList.remove("h-[552px]");
+				}
+			}
+		}
+	};
+
+	const handleBackFromCategories = (e: any) => {
+		e.stopPropagation();
+		if (categoriesRef.current) {
+			categoriesRef.current.classList.add("hidden");
+		}
+		if (window.innerWidth < 768) {
+			if (headerRef.current) {
+				headerRef.current.classList.add("z-[3]");
+			}
+		}
+	};
+
 	return (
-		<header className="relative flex items-center justify-between dark:bg-black-dark-3">
-			<div className="hidden h-12 lg:block">
-				{/* light logo */}
-				<Image
-					className="w-auto h-full aspect-auto dark:hidden"
-					src={"./images/logo/light/logo-primary-lightBg.svg"}
-					height={100}
-					width={100}
-					alt="logo"
-				/>
+		<header className="relative bg-white-light dark:bg-black-dark-3">
+			<section ref={headerRef} className="relative z-[3] flex items-center justify-between">
+				<div className="hidden h-12 lg:block">
+					{/* light logo */}
+					<Image
+						className="w-auto h-full aspect-auto dark:hidden"
+						src={"./images/logo/light/logo-primary-lightBg.svg"}
+						height={100}
+						width={100}
+						alt="logo"
+					/>
 
-				{/* dark logo */}
-				<Image
-					className="hidden w-auto h-full aspect-auto dark:lg:block"
-					src={"./images/logo/dark/logo-primary-darkBg.svg"}
-					height={100}
-					width={100}
-					alt="logo"
-				/>
-			</div>
-			<div className=" p-3 rounded-full cursor-pointer bg-gray-accent w-fit lg:absolute lg:left-[50%] lg:-translate-x-[50%] hover:bg-primary-100 group dark:bg-black-dark-2">
-				<Menu className="group-hover:text-light-100 dark:text-light-100" height={24} width={24} />
-			</div>
+					{/* dark logo */}
+					<Image
+						className="hidden w-auto h-full aspect-auto dark:lg:block"
+						src={"./images/logo/dark/logo-primary-darkBg.svg"}
+						height={100}
+						width={100}
+						alt="logo"
+					/>
+				</div>
+				<div
+					onClick={handleClickNavBarBtn}
+					className=" p-3 rounded-full cursor-pointer bg-gray-accent w-fit lg:absolute lg:left-[50%] lg:-translate-x-[50%] hover:bg-primary-100 group dark:bg-black-dark-2 transition-colors duration-300 ease-linear"
+				>
+					{showNavbar ? (
+						<Delete
+							className="group-hover:text-light-100 dark:text-light-100"
+							height={24}
+							width={24}
+						/>
+					) : (
+						<Menu
+							className="group-hover:text-light-100 dark:text-light-100"
+							height={24}
+							width={24}
+						/>
+					)}
+				</div>
 
-			<div ref={logoRef} className="absolute h-12 left-[50%] -translate-x-[50%] lg:hidden">
-				{/* light logo */}
-				<Image
-					className="w-auto h-full aspect-auto md:hidden dark:hidden"
-					src={"./images/logo/light/logo-secondary-lightBg.svg"}
-					height={100}
-					width={100}
-					alt="logo"
-				/>
+				<div ref={logoRef} className="absolute h-12 left-[50%] -translate-x-[50%] lg:hidden">
+					{/* light logo */}
+					<Image
+						className="w-auto h-full aspect-auto md:hidden dark:hidden"
+						src={"./images/logo/light/logo-secondary-lightBg.svg"}
+						height={100}
+						width={100}
+						alt="logo"
+					/>
 
-				<Image
-					className="hidden w-auto h-full aspect-auto md:block dark:hidden"
-					src={"./images/logo/light/logo-primary-lightBg.svg"}
-					height={100}
-					width={100}
-					alt="logo"
-				/>
+					<Image
+						className="hidden w-auto h-full aspect-auto md:block dark:hidden"
+						src={"./images/logo/light/logo-primary-lightBg.svg"}
+						height={100}
+						width={100}
+						alt="logo"
+					/>
 
-				{/* dark light */}
-				<Image
-					className="hidden w-auto h-full aspect-auto md:hidden dark:block md:dark:hidden"
-					src={"./images/logo/dark/logo-secondary-darkBg.svg"}
-					height={100}
-					width={100}
-					alt="logo"
-				/>
-				<Image
-					className="hidden w-auto h-full aspect-auto dark:md:block"
-					src={"./images/logo/dark/logo-primary-darkBg.svg"}
-					height={100}
-					width={100}
-					alt="logo"
-				/>
-			</div>
+					{/* dark light */}
+					<Image
+						className="hidden w-auto h-full aspect-auto md:hidden dark:block md:dark:hidden"
+						src={"./images/logo/dark/logo-secondary-darkBg.svg"}
+						height={100}
+						width={100}
+						alt="logo"
+					/>
+					<Image
+						className="hidden w-auto h-full aspect-auto dark:md:block"
+						src={"./images/logo/dark/logo-primary-darkBg.svg"}
+						height={100}
+						width={100}
+						alt="logo"
+					/>
+				</div>
 
-			<div className="flex items-center">
-				{search ? (
-					<SearchInput onClose={handleCloseSearchInput} />
-				) : (
-					<Search
+				<div className="flex items-center">
+					{search ? (
+						<SearchInput onClose={handleCloseSearchInput} />
+					) : (
+						<Search
+							className="hidden cursor-pointer md:block dark:text-light-100"
+							height={24}
+							width={24}
+							color="#000"
+							onClick={handleClickSearchIcon}
+						/>
+					)}
+
+					<CartIndicator
+						className="cursor-pointer md:mr-6 md:ml-8 dark:text-light-100"
+						height={28}
+						width={32}
+						color="#000"
+					/>
+					<Profile
 						className="hidden cursor-pointer md:block dark:text-light-100"
 						height={24}
 						width={24}
 						color="#000"
-						onClick={handleClickSearchIcon}
 					/>
-				)}
+				</div>
+			</section>
 
-				<CartIndicator
-					className="cursor-pointer md:mr-6 md:ml-8 dark:text-light-100"
-					height={28}
-					width={32}
-					color="#000"
-				/>
-				<Profile
-					className="hidden cursor-pointer md:block dark:text-light-100"
-					height={24}
-					width={24}
-					color="#000"
-				/>
-			</div>
+			{/* navbar */}
+			<section
+				ref={navbarRef}
+				className="fixed pt-[110px] top-0 bottom-0 left-0 right-0 z-[2] hidden px-8 bg-white-light md:px-10 lg:px-12 xl:px-24"
+			>
+				<SearchInput className="mb-6 md:hidden" />
+
+				<ul
+					ref={navbarListRef}
+					className="flex flex-col items-center mx-auto text-center transition-transform gap-y-6"
+				>
+					<li className="text-paragraph-1 text-primary-100">Home</li>
+					<div onClick={handleShowCategories} className="md:relative">
+						<div className="flex items-center text-paragraph-1 gap-x-4">
+							Categories
+							<GoForward height={16} width={16} stroke={white_light} fill={black_dark[1]} />
+						</div>
+						<div
+							ref={categoriesRef}
+							className="fixed z-[2] bg-white-light top-0 bottom-0 left-0 right-0 p-8 hidden transition-all ease-linear duration-300 md:absolute md:top-0 md:left-[calc(100%+70px)] md:p-0"
+						>
+							<nav className="flex justify-between md:hidden">
+								<button
+									onClick={handleBackFromCategories}
+									className="p-3 transition-colors duration-300 ease-linear rounded-full cursor-pointer bg-gray-accent w-fit hover:bg-primary-100 group dark:bg-black-dark-2"
+								>
+									<GoBack24 className="dark:text-light-100" height={24} width={24} />
+								</button>
+								<button
+									onClick={handleClickNavBarBtn}
+									className="p-3 transition-colors duration-300 ease-linear rounded-full cursor-pointer bg-gray-accent w-fit hover:bg-primary-100 group dark:bg-black-dark-2"
+								>
+									<Delete className="dark:text-light-100" height={24} width={24} />
+								</button>
+							</nav>
+							<ul className="space-y-6 text-center text-paragraph-1">
+								<li>On sale</li>
+								<li>Featured</li>
+								<li>Masks</li>
+								<li>Eye care</li>
+								<li>Moisturizers</li>
+								<li>Treatments</li>
+								<li>Night Care</li>
+								<li>Sun Care</li>
+							</ul>
+						</div>
+					</div>
+					<li className="text-paragraph-1">Blog</li>
+					<li className="text-paragraph-1">About</li>
+					<li className="text-paragraph-1">Contact</li>
+				</ul>
+
+				<PrimaryBtn title="Login" size="small" type="primary" className="w-full mt-6 mb-[68px]" />
+
+				<div className="fixed z-[2] flex gap-x-6 left-8 bottom-8 md:left-10 lg:left-12 lg:bottom-12 xl:left-24">
+					<div className="md:w-12 md:h-12 md:rounded-full md:bg-gray-accent md:dark:bg-black-dark-2 md:flex md:justify-center md:items-center">
+						<Instargram width={24} height={24} className="text-black dark:text-light-100" />
+					</div>
+					<div className="md:w-12 md:h-12 md:rounded-full md:bg-gray-accent md:dark:bg-black-dark-2 md:flex md:justify-center md:items-center">
+						<Twitter width={24} height={24} className="text-black dark:text-light-100" />
+					</div>
+					<div className="md:w-12 md:h-12 md:rounded-full md:bg-gray-accent md:dark:bg-black-dark-2 md:flex md:justify-center md:items-center">
+						<Facebook width={24} height={24} className="text-black dark:text-light-100" />
+					</div>
+				</div>
+			</section>
 		</header>
 	);
 }
