9c7ebc4875b7b8aaec8f04d958d6571d241cd7a6
VoMinhTuan21 <vominhtuan.8a1@gmail.com>
2023-01-31 10:26:40 +0700

    feat: banner home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/router";
 import { useAppSelector } from "../app/hooks";
 import { selectUser } from "../redux/slices/user-slice";
 import TitlePage from "../components/title-page/title-page";
+import Image from "next/image";
 
 export default function Home() {
 	const { data: session } = useSession();
@@ -15,13 +16,22 @@ export default function Home() {
 	const handleSignIn = () => push(`/auth/sign-in`);
 	return (
 		<section>
-			<div>
-				<TitlePage
-					className="pt-14 pb-6"
-					subtitle="Sản phẩm chăm sóc da"
-					title="Chúng tôi cung cấp những sản phẩm tốt nhất cho làn da của bạn"
-				/>
-				<Button type="primary">Mua ngay dddd</Button>
+			<div className="lg:bg-gray-accent lg:rounded-5xl lg:relative">
+				<div className="lg:ml-12 lg:absolute lg:left-0 lg:w-[400px] xl:w-[496px] xl:ml-24 lg:top-0 lg:bottom-0 lg:content-center lg:grid">
+					<TitlePage
+						className="pt-14 pb-6"
+						subtitle="Sản phẩm chăm sóc da"
+						title="Chúng tôi cung cấp những sản phẩm tốt nhất cho làn da của bạn"
+					/>
+					<Button className="lg:w-fit" type="primary">
+						Mua ngay
+					</Button>
+				</div>
+				<div className=" lg:mt-12 lg:mb-6 rounded-5xl md:py-4 lg:flex lg:justify-end bg-gray-accent lg:bg-transparent lg:rounded-none dark:bg-black-dark-2 px-2 py-10 mt-10">
+					<div className="relative h-[221px] md:h-96 lg:h-[432px] lg:w-[576px] xl:w-[768px] xl:h-[576px]">
+						<Image className="mx-auto" src={"/images/banner/banner.svg"} fill alt="banner" />
+					</div>
+				</div>
 			</div>
 		</section>
 	);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	darkMode: "class",
-	content: [
-		"./pages/**/*.{js,ts,jsx,tsx}",
-		"./components/**/*.{js,ts,jsx,tsx}",
-		"./layout/**/*.{js,ts,jsx,tsx}",
-	],
+	content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./layout/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
 			colors: {
@@ -64,6 +60,7 @@ module.exports = {
 
 			borderRadius: {
 				"4xl": "2rem",
+				"5xl": "3rem",
 			},
 
 			screens: {
