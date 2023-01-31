7d38eda771017a541a02956bccf2b4e06325c917
VoMinhTuan21 <vominhtuan.8a1@gmail.com>
2023-01-31 10:37:02 +0700

    chore: change background of banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
 	const handleSignIn = () => push(`/auth/sign-in`);
 	return (
 		<section>
-			<div className="lg:bg-gray-accent lg:rounded-5xl lg:relative">
+			<div className="lg:bg-gray-accent lg:rounded-5xl lg:dark:bg-black-dark-2 dark:lg:rounded-5xl lg:relative">
 				<div className="lg:ml-12 lg:absolute lg:left-0 lg:w-[400px] xl:w-[496px] xl:ml-24 lg:top-0 lg:bottom-0 lg:content-center lg:grid">
 					<TitlePage
 						className="pt-14 pb-6"
@@ -27,7 +27,7 @@ export default function Home() {
 						Mua ngay
 					</Button>
 				</div>
-				<div className=" lg:mt-12 lg:mb-6 rounded-5xl md:py-4 lg:flex lg:justify-end bg-gray-accent lg:bg-transparent lg:rounded-none dark:bg-black-dark-2 px-2 py-10 mt-10">
+				<div className=" lg:mt-12 lg:mb-6 rounded-5xl md:py-4 lg:flex lg:justify-end bg-gray-accent lg:bg-transparent dark:lg:bg-transparent lg:rounded-none dark:bg-black-dark-2 px-2 py-10 mt-10">
 					<div className="relative h-[221px] md:h-96 lg:h-[432px] lg:w-[576px] xl:w-[768px] xl:h-[576px]">
 						<Image className="mx-auto" src={"/images/banner/banner.svg"} fill alt="banner" />
 					</div>
