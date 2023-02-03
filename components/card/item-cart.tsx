12ab7e2b68f5d1e08424d4a55056b003287504d2
TuVoXuan <voxuantu1235@gmail.com>
2023-02-03 23:14:24 +0700

    feat: make shopping cart page

---
+++ components/card/item-cart.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Price from "../badge/price";
+import QuantityBtn from "../buttons/quantity-btn";
+import Delete from "../icons/delete";
+import ProductImage from "../Image/product-image";
+
+interface Props {
+	sale?: number;
+	price: number;
+}
+
+export default function ItemCart({ sale, price }: Props) {
+	return (
+		<div className="flex flex-col items-center p-6 border-2 rounded-4xl border-gray-accent md:p-14 md:flex-row md:gap-x-14 md:items-start lg:relative xl:static dark:border-black-dark-2">
+			<ProductImage className="h-24 w-24 !rounded-2xl md:!rounded-4xl mb-6 md:h-48 md:w-48 md:mb-0" />
+			<div>
+				<h4 className="mb-4 font-semibold text-heading-4 md:text-heading-2 dark:text-white">
+					Sleepless Night 10 g
+				</h4>
+				{/* price */}
+				<div className="flex items-center mb-10 space-x-2 md:mb-6">
+					{sale && (
+						<span className="font-semibold line-through text-heading-6 md:text-heading-4 text-dark-24 dark:text-light-24">
+							${price}
+						</span>
+					)}
+
+					<span className="font-semibold text-heading-5 md:text-paragraph-1 dark:text-white-light">
+						${sale ? (price * (100 - sale)) / 100 : price}
+					</span>
+				</div>
+
+				<div className="flex gap-x-6">
+					<QuantityBtn />
+
+					<button className="top-0 p-3 border-2 rounded-full border-gray-accent md:p-4 lg:absolute lg:right-14 lg:top-1/2 lg:-translate-y-[50%] xl:static xl:translate-y-0 dark:border-black-dark-2">
+						<Delete height={24} width={24} className="dark:text-white md:w-8 md:h-8" />
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+}
--- pages/shopping-cart.tsx
+++ pages/shopping-cart.tsx
@@ -1,5 +1,56 @@
 import React from "react";
+import Button from "../components/buttons/button";
+import ItemCart from "../components/card/item-cart";
+import TitlePage from "../components/title-page/title-page";
 
 export default function ShoppingCart() {
-	return <div>ShoppingCart</div>;
+	return (
+		<section className="pt-14 md:pt-16 mb-[104px] md:mb-[112px] xl:mb-[144px]">
+			<div className="justify-between mb-14 md:items-end md:flex md:mb-16">
+				<TitlePage className="mb-6 md:mb-0" subtitle="Giỏ hàng của bạn" title="Giỏ hàng" />
+
+				<Button type="secondary">Xóa tất cả</Button>
+			</div>
+
+			<div className="justify-between xl:flex">
+				<div className="mb-10 space-y-10 md:mb-12 md:space-y-12 xl:mb-0 xl:w-[784px]">
+					<ItemCart sale={20} price={100} />
+					<ItemCart sale={20} price={100} />
+				</div>
+
+				<div className="p-6 border-2 border-gray-accent rounded-4xl md:p-14 xl:w-[416px] dark:border-black-dark-2">
+					<h4 className="mb-10 font-semibold text-heading-4 md:text-heading-2 md:mb-12 dark:text-white">
+						Tổng số giỏ hàng
+					</h4>
+					<div className="space-y-10 mb-14 md:space-y-12 md:mb-20 lg:space-y-0 lg:grid lg:grid-cols-2 lg:gap-x-24 lg:gap-y-12 lg:mb-12 xl:block xl:space-y-12 xl:mb-20">
+						<div className="flex justify-between">
+							<p className="text-heading-5 md:text-paragraph-1 dark:text-white">Tổng phụ:</p>
+							<p className="text-heading-5 md:text-paragraph-1 dark:text-white">$209</p>
+						</div>
+						<div className="flex justify-between">
+							<p className="text-heading-5 md:text-paragraph-1 dark:text-white">Thuế:</p>
+							<p className="text-heading-5 md:text-paragraph-1 dark:text-white">$209</p>
+						</div>
+						<div className="flex justify-between">
+							<p className="text-heading-5 md:text-paragraph-1 dark:text-white">
+								Phí giao hàng:
+							</p>
+							<p className="text-heading-5 md:text-paragraph-1 dark:text-white">$209</p>
+						</div>
+						<div className="flex justify-between">
+							<p className="font-semibold text-heading-5 md:text-paragraph-1 dark:text-white">
+								Tổng:
+							</p>
+							<p className="font-semibold text-heading-5 md:text-paragraph-1 dark:text-white">
+								$209
+							</p>
+						</div>
+					</div>
+					<Button className="w-full lg:w-fit xl:w-full" type="primary">
+						Thanh toán
+					</Button>
+				</div>
+			</div>
+		</section>
+	);
 }
