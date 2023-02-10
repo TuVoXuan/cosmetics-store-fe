b43c075b45fa6d59abd6201d87c2c10759c4f7ae
VoMinhTuan21 <vominhtuan.8a1@gmail.com>
2023-02-10 10:16:03 +0700

    orders history page

---
+++ components/order/order-container.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Badge from "../badge/badge";
+import Button from "../buttons/button";
+import OrderItem from "./order-item";
+
+export default function OrderContainer() {
+	return (
+		<div className="p-4 space-y-8 border-2 md:p-12 lg:p-16 border-gray-accent rounded-3xl dark:border-black-dark-2">
+			<div className="flex items-end justify-between">
+				<h2 className="text-heading-4 md:text-heading-2 lg:text-heading-2 dark:text-light-100">#123456789</h2>
+				<p className="font-semibold text-paragraph-5 md:text-heading-5 text-dark-40 dark:text-light-100">11/2/2023</p>
+			</div>
+			<div className="space-y-8">
+				<OrderItem />
+				<OrderItem />
+			</div>
+			<div>
+				<p className="text-paragraph-5 md:text-paragraph-2 lg:text-heading-4 dark:text-light-100">
+					và <span className="font-semibold">3</span> sản phẩm khác
+				</p>
+				<div className="flex justify-between">
+					<p className="font-semibold text-paragraph-5 md:text-heading-3 lg:text-heading-4 dark:text-light-100">
+						Tổng đơn hàng:
+					</p>
+					<p className="font-semibold text-paragraph-5 md:text-heading-3 lg:text-heading-4 dark:text-light-100">
+						499.000đ
+					</p>
+				</div>
+			</div>
+			<div className="md:flex md:justify-end">
+				<Button className="w-full md:w-fit md:ml" type="danger">
+					Hủy đơn hàng
+				</Button>
+			</div>
+		</div>
+	);
+}
---
+++ components/order/order-status.tsx
@@ -0,0 +1,20 @@
+import clsx from "clsx";
+import React from "react";
+
+interface Props {
+	children: React.ReactNode;
+	active?: boolean;
+}
+
+export default function OrderStatus({ children, active }: Props) {
+	return (
+		<h4
+			className={clsx(
+				"text-heading-4 border-2 border-gray-accent rounded-4xl px-8 py-2 dark:border-black-dark-2 dark:text-light-100",
+				active && "text-light-100 border-primary-100 bg-primary-100"
+			)}
+		>
+			{children}
+		</h4>
+	);
+}
